c01cab1226be03b4dc30295e15f3892b2012af71
The-HonouredOne <surendrajunwal852@gmail.com>
2025-11-27 14:22:55 +0530

    second

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,14 +14,14 @@ const Navbar = () => {
         </div>
 
         {/* Menu */}
-        <ul className="hidden md:flex items-center gap-8 text-gray-700 font-medium">
+        {/* <ul className="hidden md:flex items-center gap-8 text-gray-700 font-medium">
           <li className="hover:text-purple-600 cursor-pointer">Home</li>
           <li className="hover:text-purple-600 cursor-pointer">About</li>
           <li className="hover:text-purple-600 cursor-pointer">Services</li>
           <li className="hover:text-purple-600 cursor-pointer">Process</li>
           <li className="hover:text-purple-600 cursor-pointer">Portfolio</li>
           <li className="hover:text-purple-600 cursor-pointer">Blog</li>
-        </ul>
+        </ul> */}
 
         {/* Button */}
         <button className="bg-purple-600 hover:bg-purple-700 transition text-white px-6 py-2 rounded-lg font-semibold">
